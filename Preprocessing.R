b4b649004ab83b6ae958d633a5bf32a1a2a49519
rymyung <rymyung@hanmail.net>
2018-09-12 16:28:51 +0900

    Revise some data'

--- Preprocessing.R
+++ Preprocessing.R
@@ -10,10 +10,10 @@
 #####################################################################################
 
 # Set working directory
-setwd("C:/Users/rymyu/Dropbox/Public/github/LoL-Recommender")
+setwd("C:/Users/Ro_Laptop/Dropbox/Public/github/LoL-Recommender")
 
 # Load libraries
-pkgs <- c("dplyr", "stringr", "tm", "qdap")
+pkgs <- c("dplyr", "tidyr")
 sapply(pkgs, require, character.only = T)
 
 
@@ -21,29 +21,36 @@ sapply(pkgs, require, character.only = T)
 ### Data load
 #####################################################################################
 
-play <- read.csv("Data/crawled_data_opgg/gameRecord.csv", stringsAsFactors = F)
+play <- read.csv("Data/crawled_data_api/play.csv", stringsAsFactors = F)
+champ <- read.csv("Data/crawled_data_api/champ.csv", stringsAsFactors = F)
 
 
 #####################################################################################
 ### Preprocessing
 #####################################################################################
 
-play$index <- 1:nrow(play)
+temp <- play %>% left_join(champ %>% select(champId, champName), by = "champId") %>%
+  select(gameId, champName, team, result)
 
-gameId_temp <- 1:nrow(play)
-gameId_list <- sample(gameId_temp, nrow(play))
-
+gameId_list <- unique(temp$gameId)
 game <- data.frame()
-p <- progress_estimated(nrow(play))
-for (i in 1:nrow(play)) {
-  temp <- play %>% filter(time == play$time[i]) %>% 
-    filter(myTeams == play$myTeams[i] || enemyTeams == play$myTeams[i]) %>%
-    mutate(team = ifelse(myTeams == play$myTeams[i], 100, 200),
-           gameId = gameId_list[i])
+p <- progress_estimated(length(gameId_list))
+for (game_id in gameId_list) {
+  temp2 <- temp %>% filter(gameId == game_id) %>% arrange(team)
+  temp3 <- data.frame(team100_1 = temp2$champName[1],
+                      team100_2 = temp2$champName[2],
+                      team100_3 = temp2$champName[3],
+                      team100_4 = temp2$champName[4],
+                      team100_5 = temp2$champName[5],
+                      team100_6 = temp2$champName[6],
+                      team100_7 = temp2$champName[7],
+                      team100_8 = temp2$champName[8],
+                      team100_9 = temp2$champName[9],
+                      team100_10 = temp2$champName[10], 
+                      result = ifelse(unique((temp2 %>% filter(team == 100))$result) == "Win", 1, 0))
   
-  game <- game %>% bind_rows(temp)
-  play <- play %>% filter(!index %in% temp$index)
-  gameId_list <- gameId_list[2:length(gameId_list)]
-  
+  game <- game %>% bind_rows(temp3)
   p$tick()$print()
 }
+
+write.csv(game, "Data/game_result.csv", row.names = F)
